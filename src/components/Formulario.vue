07aa675cf9900beb01b987a3b5a819130f37957f
Marcio Sousa <sousa-marcio@live.com>
2022-06-05 20:19:44 -0300

    Inserindo evento de click nos botões e métodos para o cronometro

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -17,15 +17,17 @@
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
           <section>
-            <strong>00:00:00</strong>
+            <strong>
+              {{ tempoDecorrido }}
+            </strong>
           </section>
-          <button class="button">
+          <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="finalizar">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -42,5 +44,26 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "FormularioPrincipal",
+  data () {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0
+    }
+  },
+  computed: {
+    tempoDecorrido () : string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+    }
+  },
+  methods: {
+    iniciar () : void {
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1
+      }, 1000)
+    },
+    finalizar () : void {
+      clearInterval(this.cronometro)
+    }
+  },
 });
 </script>
