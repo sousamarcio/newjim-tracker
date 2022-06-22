3edd2802a3b5748a004019049fed9ce99714c397
Marcio Sousa <sousa-marcio@live.com>
2022-06-22 20:51:34 -0300

    Separando componente e lógicas do Botão

---
+++ src/components/Botao.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="button">
+    <span class="icon">
+      <i :class="icone"></i>
+    </span>
+    <span>{{texto}}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BotaoTracker",
+  props: {
+    texto: {
+      type: String,
+      default: "",
+    },
+    icone: {
+      type: String,
+      default: ""
+    },
+    cronometroRodando: {
+        type: Boolean,
+        default: false
+    }
+  }
+});
+</script>
--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,50 +1,52 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <CronometroTracker :tempoEmSegundos="tempoEmSegundos" />
-    <button class="button" @click="iniciar" :disabled="cronometroRodando">
-      <span class="icon">
-        <i class="fas fa-play"></i>
-      </span>
-      <span>play</span>
-    </button>
-    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-      <span class="icon">
-        <i class="fas fa-stop"></i>
-      </span>
-      <span>stop</span>
-    </button>
+    <BotaoTracker :texto="textoBtn1" :icone="play"
+    :disabled="cronometroRodando" 
+    :cronometroRodando="cronometroRodando" 
+    @click="iniciar" />
+    <BotaoTracker :texto="textoBtn2" :icone="stop"
+     :cronometroRodando="cronometroRodando"
+     :disabled="!cronometroRodando"
+     @click="finalizar" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import CronometroTracker from "./Cronometro.vue";
+import BotaoTracker from "./Botao.vue";
 
 export default defineComponent({
   name: "TemporizadorTracker",
-  emits: ['aoTemporizadorFinalizado'],
+  emits: ["aoTemporizadorFinalizado"],
   components: {
-    CronometroTracker
+    CronometroTracker,
+    BotaoTracker
   },
   data() {
     return {
       tempoEmSegundos: 0,
       cronometro: 0,
-      cronometroRodando: false
+      cronometroRodando: false,
+      textoBtn1: 'play',
+      textoBtn2: 'stop',
+      play: 'fas fa-play',
+      stop: 'fas fa-stop'
     };
   },
   methods: {
     iniciar(): void {
-        this.cronometroRodando = true
+      this.cronometroRodando = true;
       this.cronometro = setInterval(() => {
-        this.tempoEmSegundos += 1
+        this.tempoEmSegundos += 1;
       }, 1000);
     },
     finalizar(): void {
-        this.cronometroRodando = false
-      clearInterval(this.cronometro)
-      this.$emit('aoTemporizadorFinalizado', this.tempoEmSegundos)
-      this.tempoEmSegundos = 0
+      this.cronometroRodando = false;
+      clearInterval(this.cronometro);
+      this.$emit("aoTemporizadorFinalizado", this.tempoEmSegundos);
+      this.tempoEmSegundos = 0;
     }
   }
 });
