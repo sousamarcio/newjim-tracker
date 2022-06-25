72b4e638fd67646987daccb22e4324397f1959c9
Marcio Sousa <sousa-marcio@live.com>
2022-06-25 00:33:17 -0300

    Implementando a opção de tema escuro da ferramenta via evento

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -3,6 +3,9 @@
         <h1>
             <img src="../assets/logo.png" alt="logo" />
         </h1>
+        <button class="button" @click="alterarTema">
+            {{ textoBotao }}
+        </button>
     </header>
 </template>
 
@@ -11,7 +14,27 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'BarraLateral'
+    name: 'BarraLateral',
+    emits: ['aoTemaAlterado'],
+    data () {
+        return {
+            modoEscuroAtivo: false
+        }
+    },
+    computed: {
+        textoBotao () : string {
+            if (this.modoEscuroAtivo) {
+                return 'Desativar modo escuro'
+            }
+            return 'Ativar modo escuro'
+        }
+    },
+    methods: {
+        alterarTema () : void {
+            this.modoEscuroAtivo = !this.modoEscuroAtivo
+            this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
+        }
+    }
 });
 
 </script>
@@ -22,6 +45,7 @@ header {
     background: #0d3b66;
     width: 100%;
     height: 100vh;
+    text-align: center;
 }
 @media only screen and (max-width: 768px) {
     header {
