2d456b79a873a04105a900b0fb5c239d4bca542d
Marcio Sousa <sousa-marcio@live.com>
2023-07-27 12:09:25 -0300

    feat: implements base route in app

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@
       <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
     <div class="column is-three-quarter conteudo">
-      <FormularioTracker @aoSalvarTarefa="salvarTarefa"/>
-      <div class="lista">
-        <TarefaTracker v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
-        <BoxTracker v-if="listaEstaVazia">
-          Você não está muito produtivo hoje :(
-        </BoxTracker>
-      </div>
+      <RouterView />
     </div>
   </main>
 </template>
@@ -18,35 +12,19 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
-import FormularioTracker from "./components/Formulario.vue";
-import TarefaTracker from "./components/Tarefa.vue";
-import ITarefa from './interfaces/ITarefa';
-import BoxTracker from './components/Box.vue';
 
 export default defineComponent({
   name: "App",
   components: {
     BarraLateral,
-    FormularioTracker,
-    TarefaTracker,
-    BoxTracker
   },
   data () {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false
     }
   },
-  computed: {
-    listaEstaVazia () : boolean {
-      return this.tarefas.length === 0
-    }
-  },
   methods: {
-    salvarTarefa (tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
-    },
-    trocarTema (modoEscuroAtivo: boolean) {
+    trocarTema (modoEscuroAtivo: boolean) : void {
       this.modoEscuroAtivo = modoEscuroAtivo
     }
   }
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,42 @@
+<template>
+    <FormularioTracker @aoSalvarTarefa="salvarTarefa"/>
+    <div class="lista">
+        <TarefaTracker v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        <BoxTracker v-if="listaEstaVazia">
+            Você não está muito produtivo hoje :(
+        </BoxTracker>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import FormularioTracker from "../components/Formulario.vue";
+import TarefaTracker from "../components/Tarefa.vue";
+import ITarefa from '../interfaces/ITarefa';
+import BoxTracker from '../components/Box.vue';
+
+export default defineComponent({
+    name: "App",
+    components: {
+        FormularioTracker,
+        TarefaTracker,
+        BoxTracker
+    },
+    data () {
+        return {
+        tarefas: [] as ITarefa[],
+        }
+    },
+    computed: {
+        listaEstaVazia () : boolean {
+        return this.tarefas.length === 0
+        }
+    },
+    methods: {
+        salvarTarefa (tarefa: ITarefa) {
+        this.tarefas.push(tarefa)
+        },
+    }
+});
+</script>
+  
