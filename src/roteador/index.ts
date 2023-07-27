9e1a8e1e73e5bae76d3e621f52e74928e561ac52
Marcio Sousa <sousa-marcio@live.com>
2023-07-27 12:52:35 -0300

    chore: add project route

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,12 +1,20 @@
-import Tarefas from "../views/Tarefas.vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import Tarefas from "../views/Tarefas.vue";
+import Projetos from "../views/Projetos.vue"
 
 
-const rotas: RouteRecordRaw[] = [{
-    path: '/',
-    name: 'tarefas',
-    component: Tarefas
-}]
+const rotas: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'tarefas',
+        component: Tarefas
+    },
+    {
+        path: '/projetos',
+        name: 'Projetos',
+        component: Projetos
+    },
+]
 
 
 const roteador = createRouter({
