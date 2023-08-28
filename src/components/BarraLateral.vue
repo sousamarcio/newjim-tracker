bfea5896c284fd27f44438ced38df86f5d2e3d3f
Marcio Sousa <sousa-marcio@live.com>
2023-08-28 02:33:53 -0300

    chore: change logo

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,84 +1,89 @@
 <template>
-    <header>
-        <h1>
-            <img src="../assets/logo.png" alt="logo" />
-        </h1>
-        <button class="button" @click="alterarTema">
-            {{ textoBotao }}
-        </button>
-        <nav class="panel mt-5">
-            <ul>
-                <li>
-                    <RouterLink to="/" class="link">
-                        <i class="fas fa-tasks"></i>
-                        Tarefas
-                    </RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/projetos" class="link">
-                        <i class="fas fa-project-diagram"></i>
-                        Projetos
-                    </RouterLink>
-                </li>
-            </ul>
-        </nav>
-    </header>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="logo" class="logo" />
+    </h1>
+    <button class="button" @click="alterarTema">
+      {{ textoBotao }}
+    </button>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <RouterLink to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            Tarefas
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            Projetos
+          </RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script lang="ts">
-
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'BarraLateral',
-    emits: ['aoTemaAlterado'],
-    data () {
-        return {
-            modoEscuroAtivo: false
-        }
+  name: "BarraLateral",
+  emits: ["aoTemaAlterado"],
+  data() {
+    return {
+      modoEscuroAtivo: false,
+    };
+  },
+  computed: {
+    textoBotao(): string {
+      if (this.modoEscuroAtivo) {
+        return "Desativar modo escuro";
+      }
+      return "Ativar modo escuro";
     },
-    computed: {
-        textoBotao () : string {
-            if (this.modoEscuroAtivo) {
-                return 'Desativar modo escuro'
-            }
-            return 'Ativar modo escuro'
-        }
+  },
+  methods: {
+    alterarTema(): void {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo;
+      this.$emit("aoTemaAlterado", this.modoEscuroAtivo);
     },
-    methods: {
-        alterarTema () : void {
-            this.modoEscuroAtivo = !this.modoEscuroAtivo
-            this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
-        }
-    }
+  },
 });
-
 </script>
 
 <style scoped>
 header {
-    padding: 1rem;
-    background: #0d3b66;
-    width: 100%;
-    height: 100vh;
-    text-align: center;
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
 }
+
 @media only screen and (max-width: 768px) {
-    header {
-        padding: 2.5rem;
-        height: auto;
-    }
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
 }
+
+.logo {
+  width: 227px;
+  padding-bottom: 10px;
+}
+
 .panel li {
-    margin: 8px 0;
+  margin: 8px 0;
 }
 .link {
-    color: #fff;
+  color: #fff;
 }
 .link:hover {
-    color: #FAF0CA;
+  color: #faf0ca;
 }
 .link.router-link-active {
-    color: #FAF0CA;
+  color: #faf0ca;
 }
 </style>
